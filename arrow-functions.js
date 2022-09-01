5159547f9c8c9c36cb72877443d446d740026e51
Vepp1 <vinipontarolla@gmail.com>
2022-09-01 13:51:18 +0000

    First commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,43 @@
  */
 
 // Vanilla JavaScript Function
+// function addTwoNumbers(a, b) {
+//     // Code Block
+//     return a+b;
+// }
+
+// let sum = addTwoNumbers(3, 5)
+// console.log(sum);
+
 
 // Arrow Function With Parameters
+// const addTwoNumbers = (a, b) => {
+//     // Code Block
+//     return a+b;
+// }
+
+// let sum = addTwoNumbers(3, 5)
+// console.log(sum);
 
 // Single Line Arrow Function With Parameters
+// const addTwoNumbers = (a, b) => a + b;
+
+// let sum = addTwoNumbers(3, 5)
+// console.log(sum);
 
 // Implicit Returns
+// const saySomething = message => console.log(message)
+// saySomething('Hello there!')
+
+// const sayHello = () => console.log('hello');
+// sayHello();
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+        This is a a multiline string!
+    </p>`
+)
+
+console.log(returnMultipleLines());
+
